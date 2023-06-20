055b4ecf9069460a7f1de4bad26ab72aa7e04b2d
yjlee1012 <elj308@naver.com>
2023-06-20 23:29:22 +0900

    clock JS 로직 작성

---
+++ assignment/Momentum/js/clock.js
@@ -0,0 +1,16 @@
+const clock = document.getElementById("clock");
+
+const display = (time) => {
+  return time < 10 ? `0${time}` : time;
+};
+const getCurrentTime = () => {
+  const date = new Date();
+  clock.innerHTML =
+    display(date.getHours()) +
+    ":" +
+    display(date.getMinutes()) +
+    ":" +
+    display(date.getSeconds());
+};
+
+setInterval(getCurrentTime, 1000);
