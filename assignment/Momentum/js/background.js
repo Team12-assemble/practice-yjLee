e629dd9e21ed22861ba1b5a4a1f4df736c26ed26
yjlee1012 <elj308@naver.com>
2023-06-20 23:21:51 +0900

    이미지 html,css,js 로직 수정

--- assignment/Momentum/js/background.js
+++ assignment/Momentum/js/background.js
@@ -1,6 +1,7 @@
-const images = ["img1", "img2", "img3"];
+const images = ["img1.jpg", "img2.jpg", "img3.jpg"];
 
 const randomImg = images[Math.floor(Math.random() * images.length)];
 const backgroundImg = document.createElement("img");
 backgroundImg.src = `img/${randomImg}`;
+backgroundImg.className = "bg-img";
 document.body.appendChild(backgroundImg);
