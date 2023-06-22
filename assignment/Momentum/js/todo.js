f80fdfbcf3db65620509206772269cd70d677c51
yjlee1012 <elj308@naver.com>
2023-06-22 17:11:57 +0900

    todoList scrollTop 위치 변경

--- assignment/Momentum/js/todo.js
+++ assignment/Momentum/js/todo.js
@@ -19,22 +19,18 @@ const onSubmitTodo = (e) => {
   addTodoList(todoObj);
   todoArray.push(todoObj);
   setStorage();
+  todoList.scrollTop = todoList.scrollHeight;
 };
 
 const onDeleteTodo = (todoId) => {
-  //삭제하고 다시 리스트 그려주기(x)
-  //<li> dom도 같이 삭제
   const delete_li = document.querySelector(`li#${todoId}`);
-  console.log(delete_li);
   delete_li.remove();
   todoArray = todoArray.filter((todo) => todo.id !== todoId);
   console.log("삭제후", todoArray);
 
-  //localStorage 다시 저장
   setStorage();
 };
 
-//todo <li> 그려주기
 const addTodoList = (todo) => {
   const list = document.createElement("li");
   list.innerText = todo.todo;
@@ -55,14 +51,7 @@ const addTodoList = (todo) => {
 //submit event
 todoForm.addEventListener("submit", onSubmitTodo);
 
-//처음 불러올때 todolist 그려주기
-//작성~
 todoArray.forEach((todo) => {
   console.log(todo);
   addTodoList(todo);
 });
-
-//delete event
-// const deleteBtn = document.getElementById("delete-btn");
-// console.log(deleteBtn);
-// deleteBtn.addEventListener("click", onDeleteTodo);
